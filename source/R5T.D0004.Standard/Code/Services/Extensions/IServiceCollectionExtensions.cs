29a72faeaf511d379b18412f357eebc1205ba256
MinexAutomation <minex.automation@gmail.com>
2020-05-15 01:27:48 -0700

    Added R5T.D0004.Standard standard aggregation library for the IGuidProvider service.

---
+++ source/R5T.D0004.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.D0004.Default;
+
+
+namespace R5T.D0004.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IGuidProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddGuidProvider(this IServiceCollection services)
+        {
+            services.AddDefaultGuidProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IGuidProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IGuidProvider> AddGuidProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<IGuidProvider>.New(() => services.AddGuidProvider());
+            return serviceAction;
+        }
+    }
+}
